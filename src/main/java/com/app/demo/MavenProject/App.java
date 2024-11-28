9586153c35dfa0899aafc096fbc0cf80317b9fa8
meghanath <m.meghanath003@gmail.com>
2024-11-28 11:19:20 +0530

    Demo

---
+++ src/main/java/com/app/demo/MavenProject/App.java
@@ -0,0 +1,10 @@
+package com.app.demo.MavenProject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
